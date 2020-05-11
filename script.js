fe6f4d070624d9e51507263683f7d60dc7800c99
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-11 23:05:31 +0300

    Closures

---
+++ script.js
@@ -0,0 +1,19 @@
+    //Closures - functions which are defined for example in another function
+// function generator(input) {
+//     var number = input;
+//     return function() {
+//         return number * 2;
+//     };
+// }
+// var calc = generator(900);
+// console.log(calc());
+
+// function generator(input) {
+//     var number = input;
+//     return function() {
+//         return number * 2;
+//     };
+// }
+// var calc = generator(900);
+// var calcAnother = generator(1000)
+// console.log(calc());
